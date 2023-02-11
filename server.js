f29e22a12ce6bdc5e1f695d7b86a5e5c105f88fe
temitopeohassan <temitope.hassan.2@gmail.com>
2023-02-11 08:30:56 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+const port = process.env.PORT || 5000;
+
+
+app.post('^/api1', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+ let transpoter = nodemailer.createTransport({
+    service: 'gmail',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.USER, // email
+      pass: process.env.PASSWORD, //password
+    },tls:{
+      rejectUnauthorized:false
+    }
+
+  });
+  transpoter.sendMail(options, (err, info) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  });
+  });
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
