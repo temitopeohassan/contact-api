c604b87afa11058af9682d3628c7bf89e19b0ab0
temitopeohassan <temitope.hassan.2@gmail.com>
2023-04-10 20:14:48 +0100

    enable cors

--- server.js
+++ server.js
@@ -49,6 +49,25 @@ app.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
 
+
+module.exports = (req, res) => {
+  //set header first to allow request or origin domain (value can be different)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
+
+//---- other code
+
+//Preflight CORS handler
+  if(req.method === 'OPTIONS') {
+      return res.status(200).json(({
+          body: "OK"
+      }))
+  }
+
+}
+
 const handler = (req, res) => {
   const d = new Date()
   res.end(d.toString())
